d11c45b86402ebe0e5c72f3d40f43c1716900d65
Margaret <margaretyuzheng@gmail.com>
2019-10-24 14:13:36 -0700

    la_Home page slider.

--- main.js
+++ main.js
@@ -47,3 +47,53 @@ toBeSlected('.dropdown-toggle').addEventListener('click',(e)=>{
 });
 
 
+
+
+//carousel slideshow in homepage
+sliderPrev = function () {
+  next = current;
+  --current;
+  if (current == -1) current = 2
+  displayPrev(next, current);
+}
+
+sliderNext = function () {
+  prev = current;
+  current = (++current) % n;
+  displayNext(prev, current);
+}
+
+function displayPrev(next, current) {
+  $($(".top-slider-1")[current]).css({ "left": "-100%" });
+  $($(".top-slider-1")[current]).animate({ "left": "0%" }, 2000);
+  $($(".top-slider-1")[next]).animate({ "left": "100%" }, 2000);
+}
+
+function displayNext(prev, current) {
+  $($(".top-slider-1")[current]).css({ "left": "100%" });
+  $($(".top-slider-1")[current]).animate({ "left": "0%" }, 2000);
+  $($(".top-slider-1")[prev]).animate({ "left": "-100%" }, 2000);
+}
+
+var sliderIndexes = [];
+var n = 0;
+var current = 0;
+var next = 0;
+var prev = -1;
+var myVar;
+setup = function () {
+  $(".prevSlide").click(sliderPrev);
+  $(".nextSlide").click(sliderNext);
+  n = $(".top-slider-1").length;
+  for (i = 0; i < n; i++) {
+    sliderIndexes[i] = i;
+  }
+  for (i = 0; i < n; i++) {
+    $($(".top-slider-1")[i]).css({"left":"100%"});
+  }
+  $($(".top-slider-1")[0]).css({ "left": "0%" });
+
+  myVar = setInterval(sliderNext, 5000);
+}
+
+$(document).ready(setup);
